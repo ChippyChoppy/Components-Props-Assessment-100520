75102131c77056f066cd87e4b52edcee768a890e
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-12-09 09:09:33 -0500

    commit tree d98c44027601f2dde3c5efaac697aaf895b95bfe

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -3,13 +3,24 @@ import Note from '../Components/Note';
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
-        return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
+        return [
+            { id: 1, content: "First Note" }, 
+            { id: 2, content: "Second Note" }, 
+            { id: 3, content: "Third Note" }, 
+            { id: 4, content: "Fourth Note" }
+        ]
+    }
+
+    oneNote = () => {
+        return this.apiResponse().map((noteContent) => {
+            return <Note note={noteContent} /> 
+        })
     }
 
     render() {
         return (
             <ul>
-                {}
+                {this.oneNote()}
             </ul>
         )
     }
